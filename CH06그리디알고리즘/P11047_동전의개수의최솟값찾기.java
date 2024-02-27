a1c4a3b33251cf452f9885b85445870ed8c71078
seheonnn <ho78901@naver.com>
2024-02-27 22:52:17 +0900

    그리디-동전개수의최솟값

--- CH06그리디알고리즘/P11047_동전의개수의최솟값찾기.java
+++ CH06그리디알고리즘/P11047_동전의개수의최솟값찾기.java
@@ -15,13 +15,13 @@ public class P11047_동전의개수의최솟값찾기 {
 
 		int[] A = new int[N];
 
-		for (int i = 0 ; i<N;i++) {
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			A[i] = Integer.parseInt(st.nextToken());
 		}
 
 		int count = 0;
-		for(int i = N-1;i>=0;i--) {
+		for (int i = N - 1; i >= 0; i--) {
 			count += K / A[i];
 			K = K % A[i];
 		}
